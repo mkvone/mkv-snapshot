2444ebf4724b0e74bc45809e9b3265d9a7ccbcaa
dntjd1097 <dntjd1097@gmail.com>
2024-04-17 12:59:37 +0900

    edit

--- every-3hr.sh
+++ every-3hr.sh
@@ -3,27 +3,27 @@
 # Define the directory where your scripts are located
 SCRIPT_DIR="/home/ubuntu/mkv-snapshot"
 
-# Log file path
-LOG_FILE="/home/ubuntu/mkv-snapshot/logs/every-3hr_log.txt"
+# Change directory
+cd "$SCRIPT_DIR" || exit
 
-# Check if the directory exists
-if [ -d "$SCRIPT_DIR" ]; then
-    # Change directory
-    cd "$SCRIPT_DIR" || exit
-    
-    # Loop through each script and execute it
-    for script in "${SCRIPTS[@]}"; do
-        if [ -f "$script" ]; then
-            # Execute the script
-            echo "Executing $script..."
-            ./"$script" >> "$LOG_FILE" 2>&1
-            echo "Finished executing $script."
-        else
-            echo "Error: Script $script not found."
-        fi
-    done
+# List of scripts to run
+SCRIPTS=(
+    "emoney.sh"
+    "kava.sh"
+    "kichain.sh"
+    "odin.sh"
+)
 
-    echo "All scripts executed."
-else
-    echo "Error: Directory $SCRIPT_DIR does not exist."
-fi
+# Loop through each script and execute it
+for script in "${SCRIPTS[@]}"; do
+    if [ -f "$script" ]; then
+        # Execute the script
+        echo "Executing $script..."
+        bash "$script"
+        echo "Finished executing $script."
+    else
+        echo "Error: Script $script not found."
+    fi
+done
+
+echo "All scripts executed."
