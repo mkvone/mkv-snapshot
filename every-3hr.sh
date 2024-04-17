6795864ee976d7c146ebad3f00658e8b96c828c9
dntjd1097 <dntjd1097@gmail.com>
2024-04-17 11:43:17 +0900

    crontab

--- every-3hr.sh
+++ every-3hr.sh
@@ -3,13 +3,27 @@
 # Define the directory where your scripts are located
 SCRIPT_DIR="/home/ubuntu/mkv-snapshot"
 
+# Log file path
+LOG_FILE="/home/ubuntu/mkv-snapshot/logs/every-3hr_log.txt"
+
 # Check if the directory exists
 if [ -d "$SCRIPT_DIR" ]; then
     # Change directory
     cd "$SCRIPT_DIR" || exit
-    # Find all .sh files and execute them
-    find . -maxdepth 1 -type f -name "*.sh" -exec chmod +x {} \; -exec bash {} \;
-    echo "Executed all .sh scripts in $SCRIPT_DIR"
+    
+    # Loop through each script and execute it
+    for script in "${SCRIPTS[@]}"; do
+        if [ -f "$script" ]; then
+            # Execute the script
+            echo "Executing $script..."
+            ./"$script" >> "$LOG_FILE" 2>&1
+            echo "Finished executing $script."
+        else
+            echo "Error: Script $script not found."
+        fi
+    done
+
+    echo "All scripts executed."
 else
     echo "Error: Directory $SCRIPT_DIR does not exist."
 fi
