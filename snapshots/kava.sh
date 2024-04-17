251fc7b78e1e6e0356b09dbb0d1db0c89343729a
정우성 <dntjd1097@gmail.com>
2024-04-17 20:48:45 +0900

    Update kava.sh

--- snapshots/kava.sh
+++ snapshots/kava.sh
@@ -4,7 +4,6 @@ kava tendermint unsafe-reset-all --keep-addr-book
 cd ~/.kava/
 SNAP_NAME=$(curl -s https://snapshots.mkv.one/kava/ | awk -F'"' '/href=".*kava_.*\.tar\.lz4"/ {print $2}' | sed 's/^\.\///')
 
-# curl -o - -L https://snapshots.mkv.one/kava/${SNAP_NAME} | lz4 -c -d - | tar -x -C $HOME/.kava
-aria2c -x5 -s5 -o ${SNAP_NAME} https://snapshots.mkv.one/kava/${SNAP_NAME}
-lz4 -c -d ${SNAP_NAME} | tar -x -C $HOME/.kava
+curl -o - -L https://snapshots.mkv.one/kava/${SNAP_NAME} | lz4 -c -d - | tar -x -C $HOME/.kava
+
 sudo systemctl start kava
