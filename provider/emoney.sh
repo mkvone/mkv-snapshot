2365f96c15289b363d2970dd67fff8294b685880
dntjd1097 <dntjd1097@gmail.com>
2024-04-26 11:26:47 +0900

    provide at least 2 tar.gz

--- provider/emoney.sh
+++ provider/emoney.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Configuration variables
-
 SNAP_PATH="/home/ubuntu/snapshots/emoney"
 LOG_PATH="/home/ubuntu/snapshots/logs/emoney_log.txt"
 DATA_PATH="/home/ubuntu/.emd/data/"
-PARENT_DIR="/home/ubuntu/.emd" 
-DATA_DIR_NAME="data" 
+PARENT_DIR="/home/ubuntu/.emd"
+DATA_DIR_NAME="data"
 SERVICE_NAME="emd.service"
 RPC_ADDRESS="http://localhost:36647"
+
+# Ensure necessary directories exist
+mkdir -p ${SNAP_PATH}
+mkdir -p $(dirname ${LOG_PATH})  # Creates the log directory if it doesn't exist
 
 # Function to get current time in a specific format
 now_date() {
@@ -24,8 +27,9 @@ log_this() {
 # Check if the node is fully synced
 CATCHING_UP=$(curl -s ${RPC_ADDRESS}/status | jq -r .result.sync_info.catching_up)
 CHAIN_ID=$(curl -s ${RPC_ADDRESS}/status | jq -r .result.node_info.network)
-SNAP_NAME=$(echo "${CHAIN_ID}_$(date '+%Y%m%d_%H%M').tar")
-OLD_SNAP=$(ls ${SNAP_PATH} | egrep -o "${CHAIN_ID}.*tar.lz4")
+SNAP_NAME="${CHAIN_ID}_$(date '+%Y%m%d_%H%M').tar"
+OLD_SNAP=$(ls ${SNAP_PATH} | egrep -o "${CHAIN_ID}.*tar.lz4" || echo "")
+
 if [[ "$CATCHING_UP" == "false" ]]; then
     log_this "Node is fully synced."
 
@@ -43,12 +47,20 @@ if [[ "$CATCHING_UP" == "false" ]]; then
     sudo systemctl start ${SERVICE_NAME}
     echo $? >> ${LOG_PATH}
 
-    log_this "Removing old snapshot(s):"
-    cd ${SNAP_PATH}
-    rm -fv ${OLD_SNAP} &>>${LOG_PATH}
-
+    # if [ -n "${OLD_SNAP}" ]; then
+    #     log_this "Removing old snapshot(s):"
+    #     cd ${SNAP_PATH}
+    #     rm -fv ${OLD_SNAP} &>> ${LOG_PATH}
+    # fi
+# Remove all snapshots except the latest two
+    if [ -n "${OLD_SNAP}" ]; then
+        log_this "Cleaning up old snapshots, keeping the latest two:"
+        cd ${SNAP_PATH}
+        # List all snapshot files, sort them by modification time, skip the last two files, and remove the rest
+        ls -t ${SNAP_PATH}/${CHAIN_ID}*.tar.lz4 | tail -n +2 | xargs -I {} rm -fv {} &>> ${LOG_PATH}
+    fi
     log_this "Moving new snapshot to ${SNAP_PATH}"
-    mv ${HOME}/${SNAP_NAME}.lz4 ${SNAP_PATH} &>>${LOG_PATH}
+    mv ${HOME}/${SNAP_NAME}.lz4 ${SNAP_PATH} &>> ${LOG_PATH}
 
     du -hs ${SNAP_PATH} | tee -a ${LOG_PATH}
     log_this "Done\n---------------------------\n"
--- provider/kava.sh
+++ provider/kava.sh
@@ -10,6 +10,10 @@ DATA_DIR_NAME="data"
 SERVICE_NAME="kava.service"
 RPC_ADDRESS="http://localhost:36657"
 
+# Ensure necessary directories exist
+mkdir -p ${SNAP_PATH}
+mkdir -p $(dirname ${LOG_PATH})  # Creates the log directory if it doesn't exist
+
 # Function to get current time in a specific format
 now_date() {
     echo -n $(TZ=":Europe/Moscow" date '+%Y-%m-%d_%H:%M:%S')
@@ -43,12 +47,15 @@ if [[ "$CATCHING_UP" == "false" ]]; then
     sudo systemctl start ${SERVICE_NAME}
     echo $? >> ${LOG_PATH}
 
-    log_this "Removing old snapshot(s):"
-    cd ${SNAP_PATH}
-    rm -fv ${OLD_SNAP} &>>${LOG_PATH}
+    if [ -n "${OLD_SNAP}" ]; then
+        log_this "Cleaning up old snapshots, keeping the latest two:"
+        cd ${SNAP_PATH}
+        # List all snapshot files, sort them by modification time, skip the last two files, and remove the rest
+        ls -t ${SNAP_PATH}/${CHAIN_ID}*.tar.lz4 | tail -n +2 | xargs -I {} rm -fv {} &>> ${LOG_PATH}
+    fi
 
     log_this "Moving new snapshot to ${SNAP_PATH}"
-    mv ${HOME}/${SNAP_NAME}.lz4 ${SNAP_PATH} &>>${LOG_PATH}
+    mv ${HOME}/${SNAP_NAME}.lz4 ${SNAP_PATH} &>> ${LOG_PATH}
 
     du -hs ${SNAP_PATH} | tee -a ${LOG_PATH}
     log_this "Done\n---------------------------\n"
