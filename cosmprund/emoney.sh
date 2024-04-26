8594b4d8fee871faf7871b22c9fbf8bbfdef248d
dntjd1097 <dntjd1097@gmail.com>
2024-04-26 13:09:25 +0900

    cosmprund

---
+++ cosmprund/emoney.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Configuration variables
+DATA_PATH="/home/ubuntu/.emd/data/"
+PARENT_DIR="/home/ubuntu/.emd"
+DATA_DIR_NAME="data"
+SERVICE_NAME="emd.service"
+RPC_ADDRESS="http://localhost:36647"
+CLI_COMMAND="emd"
+CATCHING_UP=$(curl -s ${RPC_ADDRESS}/status | jq -r .result.sync_info.catching_up)
+
+now_date() {
+    echo -n $(TZ=":Europe/Moscow" date '+%Y-%m-%d_%H:%M:%S')
+}
+
+# Logging function
+log_this() {
+    local logging="$@"
+    printf "|$(now_date)| $logging\n" | tee -a ${LOG_PATH}
+}
+
+if [[ "$CATCHING_UP" == "false" ]]; then
+    log_this "Node is fully synced."
+
+    log_this "Stopping ${SERVICE_NAME}"
+    sudo systemctl stop ${SERVICE_NAME}
+    echo $? >> ${LOG_PATH}
+
+    log_this "Pruning data"
+    sudo docker run -v ${DATA_PATH}:${DATA_PATH} cosmprund prune ${DATA_PATH}
+    log_this "Finish pruning"
+
+    log_this "Starting ${SERVICE_NAME}"
+    sudo systemctl start ${SERVICE_NAME}
+    echo $? >> ${LOG_PATH}
+    du -hs ${DATA_PATH} | tee -a ${LOG_PATH}
+    log_this "Done\n---------------------------\n"
+else
+    log_this "Node is still catching up. Snapshot creation skipped."
+fi
---
+++ cosmprund/installation.sh
@@ -0,0 +1,19 @@
+#other cosmos chains (like odin,emoney, etc)
+cd $HOME
+git clone https://github.com/binaryholdings/cosmprund
+cd cosmprund
+git reset --hard d3bf3d8
+sudo docker build -t cosmprund .
+
+#osmosis 
+cd $HOME 
+git clone https://github.com/czarcas7ic/cosmprund osmosis_cosmprund
+cd osmosis_cosmprund
+sudo docker build -t osmosis_cosmprund .
+
+# sudo docker stop cosmprund || true
+# sudo docker rm cosmprund || true
+# sudo docker run --name cosmprund
+# sudo docker run cosmprund prune ~/.kid/data
+# sudo docker run -v ~/.kid/data:/.kid/data cosmprund prune .kid/data
+sudo docker run -v ~/.osmosisd/data:/.osmosisd/data osmosis_cosmprund prune .osmosisd/data
