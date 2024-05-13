bd7879b8bd86b3d4f82972ecfd1340a0685f311f
dntjd1097 <dntjd1097@gmail.com>
2024-05-13 18:46:07 +0900

    fix : snapshot sentry

--- snapshots/sentry/emoney.sh
+++ snapshots/sentry/emoney.sh
@@ -8,7 +8,7 @@ SNAP_NAME=$(curl -s https://snapshots.mkv.one/emoney/ | \
     sort -t_ -k2 | \
     tail -n 1)
 
-curl -o - -L https://snapshots.mkv.one/emoney/${SNAP_NAME} | lz4 -c -d - | tar -x -C $HOME/.emd
+curl -o - -L https://snapshots.mkv.one/emoney/${SNAP_NAME} | lz4 -c -d - | tar -x -C $HOME/.emd/sentry1
 
 cp $HOME/.emd/sentry1/config/addrbook.json.bak $HOME/.emd/sentry1/config/addrbook.json
 sudo systemctl start emoney
