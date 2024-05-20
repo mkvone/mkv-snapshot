86a8902dc4af6c56be9147f2ca6056c297c4d27b
dntjd1097 <dntjd1097@gmail.com>
2024-05-20 19:29:28 +0900

    add rpc servers state_sync

--- state_sync/kichain.sh
+++ state_sync/kichain.sh
@@ -5,7 +5,7 @@ sudo systemctl stop kid
 cp $HOME/.kid/config/addrbook.json $HOME/.kid/config/addrbook.json.bak
 kid tendermint unsafe-reset-all --keep-addr-book
 SNAP_RPC="https://rpc.kichain.chaintools.tech:443"
-SNAP_RPC2="https://kichain-rpc.polkachu.com:443"
+SNAP_RPC2="https://kichain-rpc.polkachu.com:443,https://rpc-mainnet.blockchain.ki:443,https://kichain-rpc.polkachu.com:443,https://rpc-kichain-ia.cosmosia.notional.ventures:443,https://kichain-mainnet-rpc.autostake.com:443,https://kichain-rpc.lavenderfive.com:443"
 
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
 BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
--- state_sync/odin.sh
+++ state_sync/odin.sh
@@ -5,7 +5,7 @@ sudo systemctl stop odin
 cp $HOME/.odin/config/addrbook.json $HOME/.odin/config/addrbook.json.bak
 odind tendermint unsafe-reset-all --keep-addr-book
 SNAP_RPC="https://odin-rpc.polkachu.com:443"
-SNAP_RPC2="https://odin-rpc.polkachu.com:443"
+SNAP_RPC2="https://odin.rpc.m.stavr.tech:443,https://rpc.odinprotocol.io:443,https://odin-rpc.lavenderfive.com:443,https://odin-mainnet-rpc.autostake.com:443"
 
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
 BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
