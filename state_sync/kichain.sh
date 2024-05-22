7a84275b36a4c3799b3cf5e08c1e028dce997a64
dntjd1097 <dntjd1097@gmail.com>
2024-05-22 10:51:20 +0900

    st_sync

--- state_sync/kichain.sh
+++ state_sync/kichain.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
-# If SNAP_RPC does not work, use altanative address
-sudo systemctl stop kid
-cp $HOME/.kid/config/addrbook.json $HOME/.kid/config/addrbook.json.bak
+# Configuration variables
+SERVICE_NAME="kid.service"
+RPC_ADDRESS="https://rpc.kichain.chaintools.tech:443"
+RPC_SERVERS="https://kichain-rpc.polkachu.com:443,https://rpc-mainnet.blockchain.ki:443,https://kichain-rpc.polkachu.com:443,https://rpc-kichain-ia.cosmosia.notional.ventures:443,https://kichain-mainnet-rpc.autostake.com:443,https://kichain-rpc.lavenderfive.com:443"
+SNAPSHOT_DIR="$HOME/snapshots/logs"
+LOG_PATH="${SNAPSHOT_DIR}/state_sync/kid_log.txt"
+mkdir -p $(dirname ${LOG_PATH})
+
+# Helper function to get current date in a specific format
+now_date() {
+    echo -n $(TZ=":Europe/Moscow" date '+%Y-%m-%d_%H:%M:%S')
+}
+
+# Logging function
+log_this() {
+    local logging="$@"
+    printf "|$(now_date)| $logging\n" | tee -a ${LOG_PATH}
+}
+
+# Main execution
+log_this "Start\n---------------------------------------------------------\n"
+log_this "Stopping ${SERVICE_NAME}"
+sudo systemctl stop ${SERVICE_NAME}
+SERVICE_STOP_STATUS=$?
+log_this "Service stop status: $SERVICE_STOP_STATUS"
+
+log_this "Resetting emd"
 kid tendermint unsafe-reset-all --keep-addr-book
-SNAP_RPC="https://rpc.kichain.chaintools.tech:443"
-SNAP_RPC2="https://kichain-rpc.polkachu.com:443,https://rpc-mainnet.blockchain.ki:443,https://kichain-rpc.polkachu.com:443,https://rpc-kichain-ia.cosmosia.notional.ventures:443,https://kichain-mainnet-rpc.autostake.com:443,https://kichain-rpc.lavenderfive.com:443"
 
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+LATEST_HEIGHT=$(curl -s $RPC_ADDRESS/block | jq -r .result.block.header.height)
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000))
+TRUST_HASH=$(curl -s "$RPC_ADDRESS/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC2\"| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC_SERVERS,$RPC_SERVERS\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.kid/config/config.toml
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" ${CONFIG_PATH}/config.toml
 
-cp $HOME/.kid/config/addrbook.json.bak $HOME/.kid/config/addrbook.json
-sudo systemctl start kid
+
+log_this "Starting ${SERVICE_NAME}"
+sudo systemctl start ${SERVICE_NAME}
+SERVICE_START_STATUS=$?
+log_this "Service start status: $SERVICE_START_STATUS"
+
+log_this "Done\n---------------------------------------------------------\n"
--- state_sync/odin.sh
+++ state_sync/odin.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
-# If SNAP_RPC does not work, use altanative address
-sudo systemctl stop odin
-cp $HOME/.odin/config/addrbook.json $HOME/.odin/config/addrbook.json.bak
+# Configuration variables
+SERVICE_NAME="odin.service"
+RPC_ADDRESS="https://odin-rpc.polkachu.com:443"
+RPC_SERVERS="https://odin.rpc.m.stavr.tech:443,https://rpc.odinprotocol.io:443,https://odin-rpc.lavenderfive.com:443,https://odin-mainnet-rpc.autostake.com:443"
+SNAPSHOT_DIR="$HOME/snapshots/logs"
+LOG_PATH="${SNAPSHOT_DIR}/state_sync/odin_log.txt"
+mkdir -p $(dirname ${LOG_PATH})
+
+# Helper function to get current date in a specific format
+now_date() {
+    echo -n $(TZ=":Europe/Moscow" date '+%Y-%m-%d_%H:%M:%S')
+}
+
+# Logging function
+log_this() {
+    local logging="$@"
+    printf "|$(now_date)| $logging\n" | tee -a ${LOG_PATH}
+}
+
+# Main execution
+log_this "Start\n---------------------------------------------------------\n"
+log_this "Stopping ${SERVICE_NAME}"
+sudo systemctl stop ${SERVICE_NAME}
+SERVICE_STOP_STATUS=$?
+log_this "Service stop status: $SERVICE_STOP_STATUS"
+
+log_this "Resetting emd"
 odind tendermint unsafe-reset-all --keep-addr-book
-SNAP_RPC="https://odin-rpc.polkachu.com:443"
-SNAP_RPC2="https://odin.rpc.m.stavr.tech:443,https://rpc.odinprotocol.io:443,https://odin-rpc.lavenderfive.com:443,https://odin-mainnet-rpc.autostake.com:443"
 
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+LATEST_HEIGHT=$(curl -s $RPC_ADDRESS/block | jq -r .result.block.header.height)
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000))
+TRUST_HASH=$(curl -s "$RPC_ADDRESS/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC2\"| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC_SERVERS,$RPC_SERVERS\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.odin/config/config.toml
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" ${CONFIG_PATH}/config.toml
 
-cp $HOME/.odin/config/addrbook.json.bak $HOME/.odin/config/addrbook.json
-sudo systemctl start odin
+
+log_this "Starting ${SERVICE_NAME}"
+sudo systemctl start ${SERVICE_NAME}
+SERVICE_START_STATUS=$?
+log_this "Service start status: $SERVICE_START_STATUS"
+
+log_this "Done\n---------------------------------------------------------\n"
