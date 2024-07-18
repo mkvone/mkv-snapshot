2a32e7114a7843f52e0c178a439799d58d9663b1
dntjd1097 <dntjd1097@gmail.com>
2024-07-18 21:57:50 +0900

    fix

--- snapshots/sentry/kava.sh
+++ snapshots/sentry/kava.sh
@@ -8,11 +8,9 @@ sudo systemctl stop ${DAEMON}
 rm -rf $SNAP_PATH/data/*
 
 SNAP_NAME=$(curl -s ${SNAP_URL}/${CHAIN}/ | \
-    awk -F'"' '/href=".*${CHAIN}.*\.tar\.lz4"/ {print $2}' | \
+    awk -F'"' '/href=".*.*\.tar\.lz4"/ {print $2}' | \
     sed 's/^\.\///' | \
     sort -t_ -k2 | \
     tail -n 1)
-
 curl -o - -L ${SNAP_URL}/${CHAIN}/${SNAP_NAME} | lz4 -c -d - | tar -x -C $SNAP_PATH
-
 sudo systemctl start ${DAEMON}
--- snapshots/sentry/odin.sh
+++ snapshots/sentry/odin.sh
@@ -3,16 +3,13 @@ CHAIN=odin
 SNAP_PATH=$HOME/.sentry/.odind
 SNAP_URL="https://snapshots.mkv.one/mainnet"
 
-
 sudo systemctl stop ${DAEMON}
 rm -rf $SNAP_PATH/data/*
 
 SNAP_NAME=$(curl -s ${SNAP_URL}/${CHAIN}/ | \
-    awk -F'"' '/href=".*${CHAIN}.*\.tar\.lz4"/ {print $2}' | \
+    awk -F'"' '/href=".*.*\.tar\.lz4"/ {print $2}' | \
     sed 's/^\.\///' | \
     sort -t_ -k2 | \
     tail -n 1)
-
 curl -o - -L ${SNAP_URL}/${CHAIN}/${SNAP_NAME} | lz4 -c -d - | tar -x -C $SNAP_PATH
-
 sudo systemctl start ${DAEMON}
