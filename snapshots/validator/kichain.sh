2eccbcb16dab9ca0766f357618cab2d6d9658430
dntjd1097 <dntjd1097@gmail.com>
2024-07-18 23:12:12 +0900

    add : snapshots for .validator folder script

---
+++ snapshots/validator/kichain.sh
@@ -0,0 +1,15 @@
+DAEMON=validator-kid
+CHAIN=kichain
+SNAP_PATH=$HOME/.validator/.kid
+SNAP_URL="https://snapshots.mkv.one/mainnet"
+
+sudo systemctl stop ${DAEMON}
+rm -rf $SNAP_PATH/data/*
+
+SNAP_NAME=$(curl -s ${SNAP_URL}/${CHAIN}/ | \
+    awk -F'"' '/href=".*.*\.tar\.lz4"/ {print $2}' | \
+    sed 's/^\.\///' | \
+    sort -t_ -k2 | \
+    tail -n 1)
+curl -o - -L ${SNAP_URL}/${CHAIN}/${SNAP_NAME} | lz4 -c -d - | tar -x -C $SNAP_PATH
+sudo systemctl start ${DAEMON}
