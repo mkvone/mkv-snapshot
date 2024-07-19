bbe536a8dccdafc10e93878e18d5c52c0aa931b3
dntjd1097 <dntjd1097@gmail.com>
2024-07-19 17:45:39 +0900

    chore : download file

--- snapshots/sentry/emoney.sh
+++ snapshots/sentry/emoney.sh
@@ -13,5 +13,5 @@ wget -O ${SNAP_PATH}/${SNAP_NAME} ${SNAP_URL}/${CHAIN}/${SNAP_NAME}
 sudo systemctl stop ${DAEMON}
 rm -rf $SNAP_PATH/data/*
 lz4 -c -d ${SNAP_PATH}/${SNAP_NAME} | tar -x -C $SNAP_PATH
-# curl -o - -L ${SNAP_URL}/${CHAIN}/${SNAP_NAME} | lz4 -c -d - | tar -x -C $SNAP_PATH
+rm -rf ${SNAP_PATH}/${SNAP_NAME}
 sudo systemctl start ${DAEMON}
